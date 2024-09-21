ad637b7ff0ad3f53a17bd44fb2512b7e39b5bec3
developer_ken <dengbw01@outlook.com>
2024-09-21 19:28:45 +0800

    基本功能完成，有待测试

--- LiveEventMarker/Program.cs
+++ LiveEventMarker/Program.cs
@@ -4,6 +4,7 @@ using LiveEventMarker.Model;
 using QRCoder;
 using System.Diagnostics;
 using System.Net;
+using System.Text;
 
 namespace LiveEventMarker
 {
@@ -32,45 +33,83 @@ namespace LiveEventMarker
             dc.LiveEndEvent += Dc_LiveEndEvent;
             Console.WriteLine("弹幕连接成功");
             BiliSpaceDynamic biliDyn = new BiliSpaceDynamic(UserId, bsession);
-
             while (true)
             {
-                Console.WriteLine("抓取当前动态...");
-                var latestDyn = biliDyn.getLatest();
-                foreach (var dyn in latestDyn)
+                try
                 {
-                    List<DateTime> delList = new List<DateTime>();
-                    if (!dyn.dynamic.Contains("录播")) continue; //不是录播，排除
-                    foreach (var liv in lives)
+                    Console.WriteLine($"{DateTime.Now.ToString("yyyyMMdd-HH:mm:ss")} 抓取当前动态...");
+                    var latestDyn = biliDyn.getLatest();
+                    foreach (var dyn in latestDyn)
                     {
-                        bool isAllFinished = true;
-                        foreach (var seg in liv.Value)
+                        List<DateTime> delList = new List<DateTime>();
+                        if (!dyn.dynamic.Contains("录播")) continue; //不是录播，排除
+                        foreach (var liv in lives)
                         {
-                            if (seg.DataPending)
+                            bool isAllFinished = true;
+                            foreach (var seg in liv.Value)
                             {
-                                isAllFinished = false;
-                                break;
+                                if (seg.DataPending)
+                                {
+                                    isAllFinished = false;
+                                    break;
+                                }
                             }
-                        }
-                        if (!isAllFinished)
-                            continue;
+                            if (!isAllFinished)
+                                continue;
 
-                        string datestr = liv.Key.ToString("yyyyMMdd");
-                        if (!dyn.vinfo.discription.Contains(datestr))
-                            continue; //录播日期不对，排除
-                        
+                            string datestr = liv.Key.ToString("yyyyMMdd");
+                            if (!dyn.vinfo.discription.Contains(datestr))
+                                continue; //录播日期不对，排除
+
+                            Console.WriteLine("找到" + datestr + "对应视频！编辑评论...");
+                            Console.WriteLine("----------------------------------------");
+                            {
+                                // 就是你了，发评论！
+                                StringBuilder sb = new StringBuilder();
+                                sb.AppendLine("<萌刷高光时刻>");
+                                sb.AppendLine("[SC]");
+                                foreach (var seg in liv.Value)
+                                {
+                                    foreach (var sc in seg.Superchat)
+                                    {
+                                        var dur = sc.Time - seg.startTime;
+                                        string durstr = dur.TotalHours >= 1 ? ($"{dur.Hours}:{dur.Minutes}:{dur.Seconds}") : ($"{dur.Minutes}:{dur.Seconds}");
+                                        sb.AppendLine($"{durstr} {sc.Name}");
+                                    }
+                                }
+                                sb.AppendLine("[舰队]");
+                                foreach (var seg in liv.Value)
+                                {
+                                    foreach (var gb in seg.Guardbuy)
+                                    {
+                                        var dur = gb.Time - seg.startTime;
+                                        string durstr = dur.TotalHours >= 1 ? ($"{dur.Hours}:{dur.Minutes}:{dur.Seconds}") : ($"{dur.Minutes}:{dur.Seconds}");
+                                        sb.AppendLine($"{durstr} {gb.Name}");
+                                    }
+                                }
+                                sb.AppendLine("\n上述信息自动生成 by @亿只鸡蛋_Egg");
+                                Console.WriteLine(sb.ToString());
+                                Console.WriteLine("----------------------------------------");
+                                Console.WriteLine("提交评论...");
+                                // 发射！
+                                var result = bsession.sendComment(dyn.vinfo.av, sb.ToString());
+                                Console.WriteLine(result.Substring(128));
+                            }
+                            delList.Add(liv.Key);
+                        }
+
+                        // 移除操作过的记录
+                        foreach (var del in delList)
                         {
-                            // 就是你了，发评论！
-
+                            lives.Remove(del);
                         }
-                        delList.Add(liv.Key);
                     }
-
-                    // 移除操作过的记录
-                    foreach(var del in delList)
-                    {
-                        lives.Remove(del);
-                    }
+                    Console.WriteLine($"{DateTime.Now.ToString("yyyyMMdd-HH:mm:ss")} 本轮操作结束。");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                    Console.WriteLine($"Stack: {e.StackTrace}");
                 }
                 Thread.Sleep(30 * 1000);            //每半分钟抓取一次新动态
             }
@@ -113,18 +152,32 @@ namespace LiveEventMarker
                     Superchat = new List<Event>(),
                 };
                 live.Add(CurrentSegment);
-                Console.WriteLine($"  + 新片段{segStart.ToString("yyyy-MM-dd-HH:mm:ss")}");
+                Console.WriteLine($" + 新片段{segStart.ToString("yyyy-MM-dd-HH:mm:ss")}");
             }
         }
 
         private static void Dc_GuardBuy(object sender, BiliveDanmakuAgent.Model.DanmakuReceivedEventArgs e)
         {
-
+            Console.WriteLine($"  + 新舰队 {e.Danmaku.UserName}#{e.Danmaku.UserID}");
+            CurrentSegment.Guardbuy.Add(
+                new Event
+                {
+                    Name = e.Danmaku.UserName,
+                    Time = DateTime.Now,
+                    Uid = e.Danmaku.UserID,
+                });
         }
 
         private static void Dc_Superchat(object sender, BiliveDanmakuAgent.Model.DanmakuReceivedEventArgs e)
         {
-
+            Console.WriteLine($"  + 新SC {e.Danmaku.UserName}#{e.Danmaku.UserID}");
+            CurrentSegment.Superchat.Add(
+                new Event
+                {
+                    Name = e.Danmaku.UserName,
+                    Time = DateTime.Now,
+                    Uid = e.Danmaku.UserID,
+                });
         }
 
         static void BLogin()
