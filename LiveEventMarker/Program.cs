c12ad760d4e3a1009f87680bc6e7688c578d9c7b
developer_ken <dengbw01@outlook.com>
2024-09-21 16:47:19 +0800

    添加项目文件。

---
+++ LiveEventMarker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LiveEventMarker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
